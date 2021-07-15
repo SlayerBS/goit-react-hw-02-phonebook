c703f4d7dd9513688a65ffd16a6ef9a5f8579d44
Sergii Bogush <sergbogush@gmail.com>
2021-07-15 19:47:58 +0300

    add submit and reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div></div>;
-}
+import React, { Component } from "react";
+import Container from "./components/Container/Container";
+import Form from "./components/Form/Form";
 
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHundler = (data) => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.formSubmitHundler} />
+      </Container>
+    );
+  }
+}
 export default App;
